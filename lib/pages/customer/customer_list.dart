5ed5166517b86938ede007370f6f4009b90bfe25
Muhamad Tajudin Subki <mtajudin98@gmail.com>
2023-02-05 12:54:12 +0700

    menambahkan aksi hapus data

--- lib/pages/customer/customer_list.dart
+++ lib/pages/customer/customer_list.dart
@@ -24,6 +24,13 @@ class _CustomerListState extends State<CustomerList> {
     setState(() {});
   }
 
+  Future<bool> hapusData(int id) async {
+    // ignore: no_leading_underscores_for_local_identifiers
+    final _db = await DBHelper.db();
+    final count = await _db?.delete('customer', where: 'id=?', whereArgs: [id]);
+    return count! > 0;
+  }
+
   Widget item(Map d) => ListTile(
         onLongPress: () {
           showMenu(
@@ -34,7 +41,11 @@ class _CustomerListState extends State<CustomerList> {
                 const PopupMenuItem(
                   value: 'S',
                   child: Text('Edit Data'),
-                )
+                ),
+                const PopupMenuItem(
+                  value: 'H',
+                  child: Text('Hapus Data'),
+                ),
               ]).then((value) {
             if (value == 'S') {
               Navigator.push(context,
@@ -42,6 +53,27 @@ class _CustomerListState extends State<CustomerList> {
                   .then((value) {
                 if (value == true) refresh();
               });
+            } else if (value == 'H') {
+              showDialog(
+                  context: context,
+                  builder: (c) => AlertDialog(
+                        content: Text('Customer ${d['nama']} ingin dihapus'),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                hapusData(d['id']).then((value) {
+                                  if (value == true) refresh();
+                                });
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Ya, Saya yakin menghapus')),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Tidak jadi hapus')),
+                        ],
+                      ));
             }
           });
         },
